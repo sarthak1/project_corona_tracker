398408f75f8ca67a638e7677775686b8dcf235a1
sarthak kanungo <skanungodgacm@gmail.com>
2020-04-07 20:29:15 +0530

    Updates

---
+++ precache-manifest.2c457395f464623bea643e3530e9c600.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "635e86cbd067a7ef91da00f2fe707e9f",
+    "url": "/project_corona_tracker/index.html"
+  },
+  {
+    "revision": "53b815835e3d67e5ea30",
+    "url": "/project_corona_tracker/static/css/main.283eb949.chunk.css"
+  },
+  {
+    "revision": "29013984e4046506ebc0",
+    "url": "/project_corona_tracker/static/js/2.d1481e41.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/project_corona_tracker/static/js/2.d1481e41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53b815835e3d67e5ea30",
+    "url": "/project_corona_tracker/static/js/main.6bad43d0.chunk.js"
+  },
+  {
+    "revision": "873ab1951ece1116043a",
+    "url": "/project_corona_tracker/static/js/runtime-main.f85e4bbc.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/project_corona_tracker/static/media/image.d7265326.png"
+  }
+]);
